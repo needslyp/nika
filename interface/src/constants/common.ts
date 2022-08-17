2d2551190e4ef3510baf898b7c3491bf903d7ff3
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-17 13:44:53 +0300

    feat: remove unused statics

--- interface/src/constants/common.ts
+++ interface/src/constants/common.ts
@@ -2,14 +2,6 @@ const defaultSocketUrl = 'ws://localhost:8090';
 const defaultScUrl = 'ws://localhost:8090';
 const defaultScScWebUrl = 'http://localhost:8000'
 
-export const SocketActions = {
-    REQUEST_START_CALL: 'request-start-call',
-    RESET_CALL: 'reset-call',
-    CANDIDATE: 'candidate',
-    OFFER: 'offer',
-    ANSWER: 'answer',
-};
-
 export const SC_URL = process.env.SC_URL ?? defaultScUrl;
 export const SOCKET_URL = process.env.SOCKET_URL ?? defaultSocketUrl;
 export const SC_WEB_URL = process.env.SC_WEB_URL ?? defaultScScWebUrl;
--- interface/src/hooks/index.ts
+++ interface/src/hooks/index.ts
@@ -1,6 +1,4 @@
 export * from './redux';
 export * from './useBooleanState';
-export * from './useNavigation';
-export * from './useUserMedia';
 export * from './useClickOutside';
 export * from './useWindowSize';
