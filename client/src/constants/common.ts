662e4436bf9ca693c13f99efdea11838d7972aff
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-17 11:32:20 +0300

    feat(interface): add scg-viewer

--- client/src/constants/common.ts
+++ client/src/constants/common.ts
@@ -1,9 +1,6 @@
 const defaultSocketUrl = 'ws://localhost:8090';
 const defaultScUrl = 'ws://localhost:8090';
-<<<<<<< HEAD
 const defaultScScWebUrl = 'http://localhost:8000'
-=======
->>>>>>> 92191d324... feat(interface): remake static
 
 export const SocketActions = {
     REQUEST_START_CALL: 'request-start-call',
@@ -15,7 +12,4 @@ export const SocketActions = {
 
 export const SC_URL = process.env.SC_URL ?? defaultScUrl;
 export const SOCKET_URL = process.env.SOCKET_URL ?? defaultSocketUrl;
-<<<<<<< HEAD
 export const SC_WEB_URL = process.env.SC_WEB_URL ?? defaultScScWebUrl;
-=======
->>>>>>> 92191d324... feat(interface): remake static
