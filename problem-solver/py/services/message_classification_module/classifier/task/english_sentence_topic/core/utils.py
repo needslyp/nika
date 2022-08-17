6769842f42f05217e6ef14b0bf2020d0d4659106
MksmOrlov <tawnhall8.5@gmail.com>
2022-08-17 17:53:34 +0300

    refactor: remove python

--- problem-solver/py/services/message_classification_module/classifier/task/english_sentence_topic/core/utils.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2021 Intelligent Semantic Systems LLC, All rights reserved.
-Author Mikhno Egor
-"""
-
-
-def is_built_in(value: str) -> bool:
-    return value.startswith("wit$")
