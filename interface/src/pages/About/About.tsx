c910a07226693af1e3138e6c337cbaa1f13c605d
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-17 20:14:33 +0300

    feat(interface): design about page

--- interface/src/pages/About/About.tsx
+++ interface/src/pages/About/About.tsx
@@ -4,11 +4,12 @@ export const About = () => {
     return (
         <div className="about-page-container">
             <div className="about-page">
-                <h1 className="about-page-text">
-                    Обучающая диалоговая экспертная система NIKA, разработанный на основе технологии{' '}
+                <h1 className="about-page-text" style={{textAlign: "justify", marginLeft: "100px", marginRight: "100px"}}>
+                    Обучающая диалоговая экспертная система <b>NIKA</b> (<b>N</b>ika is an <b>I</b>ntelligent <b>K</b>nowledge-driven <b>A</b>ssistant),
+                    разработанный на основе технологии{' '}
                         <a href="http://ims.ostis.net/" className="text">
                           OSTIS
-                        </a>{' '}
+                        </a>
                     . Обучающая диалоговая экспертная ostis-система NIKA полностью заменяет участие Обучающей
                     диалоговой экспертной системы ЭКО в образовательном процессе по обучению студентов
                     специальностей Белорусского государственного университета информатики и радиоэлектроники
