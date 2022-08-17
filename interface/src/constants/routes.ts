f03efd0de59ca3ac0a7fb00f49aa731a95bb2f16
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-17 13:44:53 +0300

    feat: remove unused statics

--- interface/src/constants/routes.ts
+++ interface/src/constants/routes.ts
@@ -1,9 +1,1 @@
-export const LOGIN = '/login' as const;
 export const DEMO = '/demo' as const;
-
-// patient routes
-export const CHAT_WITH_ASSISTANT = '/chat' as const;
-
-// assistant routes
-export const PATIENTS = '/patients' as const;
-export const CHAT_WITH_PATIENT = `${PATIENTS}/:id` as const;
