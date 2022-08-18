9b0cc35468e23d2efd1aea930df8228d789ecc48
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-18 18:30:19 +0300

    fix(interface): remove notifications

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,9 +1,7 @@
-import { lazy, useEffect } from "react";
+import { lazy } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { loadingComponent } from '@components/LoadingComponent';
 import { routes } from '@constants';
-import { useToast } from '@components/Toast';
-import { client } from '@api';
 
 import 'antd/dist/antd.css';
 import './assets/main.css';
@@ -11,7 +9,6 @@ import './assets/main.css';
 import { Layout } from 'antd';
 const { Header, Content, Footer } = Layout;
 
-import { Notification } from '@components/Notification';
 import { HeaderPanel } from "@components/Header";
 import { FooterPanel } from "@components/Footer";
 
@@ -20,7 +17,7 @@ const About = loadingComponent(lazy(() => import('@pages/About')));
 
 const DemoRoutes = () => (
     <>
-        <Route exact path={routes.MAIN}>
+        <Route path={routes.MAIN}>
             <Demo />
         </Route>
         <Redirect to={routes.MAIN} />
@@ -35,29 +32,6 @@ const AboutRoutes = () => (
     </>
 );
 
-const ClientNotifications = () => {
-    const { addToast } = useToast();
-
-    useEffect(() => {
-        const onOpen = () => {
-            console.log('::open::');
-        };
-        const onError = () => {
-            const params = { id: 'netError', duration: 2000 };
-            addToast(<Notification type="error" title="Connection lost" />, params);
-        };
-        client.addEventListener('open', onOpen);
-        client.addEventListener('error', onError);
-
-        return () => {
-            client.removeEventListener('open', onOpen);
-            client.removeEventListener('error', onError);
-        };
-    }, [addToast]);
-
-    return null;
-};
-
 export const App = () => {
     return (
         <Layout>
@@ -65,7 +39,6 @@ export const App = () => {
                 <HeaderPanel />
             </Header>
             <Content>
-                <ClientNotifications />
                 <DemoRoutes />
                 <AboutRoutes />
             </Content>
